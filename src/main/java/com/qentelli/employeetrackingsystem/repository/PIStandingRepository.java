f1bc2a6b59c4a5dcf39207d3080ea31fd6b6321f
MadhuMarrapu <104364166+MadhuMarrapu@users.noreply.github.com>
2025-08-05 13:39:57 +0530

    Update PIStandingRepository.java

--- src/main/java/com/qentelli/employeetrackingsystem/repository/PIStandingRepository.java
+++ src/main/java/com/qentelli/employeetrackingsystem/repository/PIStandingRepository.java
@@ -1,16 +1,20 @@
 package com.qentelli.employeetrackingsystem.repository;
 
+import java.util.List;
+
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.qentelli.employeetrackingsystem.entity.PIStanding;
 
-public interface PIStandingRepository extends JpaRepository<PIStanding, Long>{
-  
+public interface PIStandingRepository extends JpaRepository<PIStanding, Long> {
+
+	List<PIStanding> findBySoftDeleteFalse();
+
 	Page<PIStanding> findBySoftDeleteFalse(Pageable pageable);
-	 
-    Page<PIStanding> findByPiNumberAndSoftDeleteFalse(int piNumber, Pageable pageable);
- 
-    Page<PIStanding> findByProject_ProjectIdAndSoftDeleteFalse(int projectId, Pageable pageable);
+
+	Page<PIStanding> findByPiNumberAndSoftDeleteFalse(int piNumber, Pageable pageable);
+
+	Page<PIStanding> findByProject_ProjectIdAndSoftDeleteFalse(int projectId, Pageable pageable);
 }
