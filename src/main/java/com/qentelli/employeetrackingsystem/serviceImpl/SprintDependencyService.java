311c3f2de4de207b4e9da5f3f21bd7462a057e35
Akyellapu <104199493+Akyellapu@users.noreply.github.com>
2025-08-06 15:39:15 +0530

    Update SprintDependencyService.java

--- src/main/java/com/qentelli/employeetrackingsystem/serviceImpl/SprintDependencyService.java
+++ src/main/java/com/qentelli/employeetrackingsystem/serviceImpl/SprintDependencyService.java
@@ -1,20 +1,25 @@
 package com.qentelli.employeetrackingsystem.serviceImpl;
 
 import org.springframework.beans.BeanUtils;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
 import com.qentelli.employeetrackingsystem.entity.Project;
+import com.qentelli.employeetrackingsystem.entity.Sprint;
 import com.qentelli.employeetrackingsystem.entity.SprintDependency;
 import com.qentelli.employeetrackingsystem.entity.TaskStatus;
+import com.qentelli.employeetrackingsystem.exception.ProjectNotFoundException;
 import com.qentelli.employeetrackingsystem.exception.ResourceNotFoundException;
+import com.qentelli.employeetrackingsystem.exception.SprintNotFoundException;
 import com.qentelli.employeetrackingsystem.models.client.request.SprintDependencyRequest;
 import com.qentelli.employeetrackingsystem.models.client.response.SprintDependencyResponse;
 import com.qentelli.employeetrackingsystem.repository.ProjectRepository;
-import com.qentelli.employeetrackingsystem.repository.SprintRepository;
 import com.qentelli.employeetrackingsystem.repository.SprintDependencyRepository;
+import com.qentelli.employeetrackingsystem.repository.SprintRepository;
+import com.qentelli.employeetrackingsystem.service.SprintDependencyService;
+
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 
 
