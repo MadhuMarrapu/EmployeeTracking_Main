7b987a091a7e05baaac038dd10cc44247b6ef655
Akyellapu <104199493+Akyellapu@users.noreply.github.com>
2025-08-07 11:29:19 +0530

    Update JpaAuditConfig.java

--- src/main/java/com/qentelli/employeetrackingsystem/config/JpaAuditConfig.java
+++ src/main/java/com/qentelli/employeetrackingsystem/config/JpaAuditConfig.java
@@ -1,5 +1,7 @@
 package com.qentelli.employeetrackingsystem.config;
 
+import java.util.Map;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.domain.AuditorAware;
@@ -8,10 +10,15 @@ import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 @Configuration
 @EnableJpaAuditing(auditorAwareRef = "auditorAwareImpl")
 public class JpaAuditConfig {
- 
-	  @Bean
-	    public AuditorAware<String> auditorAware() {
-	        return new AuditorAwareImpl(); // see below
-	    }
-	
+
+    private final Map<String, Map<String, String>> adminMetadata;
+
+    public JpaAuditConfig(Map<String, Map<String, String>> adminMetadata) {
+        this.adminMetadata = adminMetadata;
+    }
+
+    @Bean
+    public AuditorAware<String> auditorAwareImpl() {
+        return new AuditorAwareImpl(adminMetadata);
+    }
 }
