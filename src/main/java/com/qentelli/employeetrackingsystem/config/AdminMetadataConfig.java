add5bbb125f3aa31c26013d19fb4cf062a1a30ac
Akyellapu <104199493+Akyellapu@users.noreply.github.com>
2025-08-07 11:28:08 +0530

    Update AdminMetadataConfig.java

--- src/main/java/com/qentelli/employeetrackingsystem/config/AdminMetadataConfig.java
+++ src/main/java/com/qentelli/employeetrackingsystem/config/AdminMetadataConfig.java
@@ -8,15 +8,16 @@ import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class AdminMetadataConfig {
-
+	
+	private static final String FIRST_NAME = "firstName";
+	private static final String LAST_NAME = "lastName";
+	
 	@Bean
 	public Map<String, Map<String, String>> adminMetadata() {
 		Map<String, Map<String, String>> details = new HashMap<>();
-
-		details.put("superadmin@gmail.com", Map.of("firstName", "Sarath", "lastName", "Tenneti"));
-		details.put("superadmin2@gmail.com", Map.of("firstName", "Madhu", "lastName", "Marrapu"));
-		details.put("Anil@qentelli.com", Map.of("firstName", "Anil", "lastName", "Kumar"));
-
+		details.put("superadmin@gmail.com", Map.of(FIRST_NAME, "Sarath", LAST_NAME, "Tenneti"));
+		details.put("superadmin2@gmail.com", Map.of(FIRST_NAME, "Madhu", LAST_NAME, "Marrapu"));
+		details.put("Anil@qentelli.com", Map.of(FIRST_NAME, "Anil", LAST_NAME, "Kumar"));
 		return details;
 	}
 }
